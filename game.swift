beb82fd33c0aef9d91ebe1c0e498f77d23bd9710
BOUHADEB Yacoub <yac512@hotmail.fr>
2018-09-08 22:47:15 +0200

    advanced implementation of class Character and each heroes class

--- game.swift
+++ game.swift
@@ -7,60 +7,95 @@
 
 import Foundation
 
+class Player {
+    var playerName: String
+    var heroes = [Character]()
+    
+    init(playerName: String) {
+        self.playerName = playerName
+    }
+    
+}
+
 class Character {
     
     var name: String
+    var attack: Int
+    var lifePoint: Int
     
-    init (name: String) {
+    init (name: String, lifePoint: Int, attack: Int) {
         self.name = name
-    }
-    
-    enum Class {
-        case warrior
-        case mage
-        case coloss
-        case dwarf
-        case paladin
-    }
-    
-    func attack() {
-        
+        self.lifePoint = lifePoint
+        self.attack = attack
     }
 }
 
 class Warrior: Character {
-    var lifePoint = 100
-    let attack = 10
-    var typeClass = Class.warrior
+   
+   override init (name: String, lifePoint: Int, attack: Int) {
+    super.init(name: name, lifePoint: lifePoint, attack: attack)
+    }
+    convenience init(name: String) {
+        self.init(name: name, lifePoint: 33, attack: 42)
+    }
     
 }
 
+
 class Mage: Character {
-    var lifePoint = 75
-    let heal = 15
-    var typeClass = Class.mage
+    static let heal = 15;
     
+    override init (name: String, lifePoint: Int, attack: Int) {
+        super.init(name: name, lifePoint: lifePoint, attack: attack)
+    }
+    convenience init(name: String) {
+        self.init(name: name, lifePoint: 33, attack: 0)
+    }
 }
+
 class Coloss: Character {
-    var lifePoint = 150
-    let attack = 5
-    var typeClass = Class.coloss
+    override init (name: String, lifePoint: Int, attack: Int) {
+        super.init(name: name, lifePoint: lifePoint, attack: attack)
+    }
+    convenience init(name: String) {
+        self.init(name: name, lifePoint: 150, attack: 6)
+    }
     
 }
 
 class Dwarf: Character {
-    var lifePoint = 75
-    let attack = 15
-    var typeClass = Class.dwarf
+    override init (name: String, lifePoint: Int, attack: Int) {
+        super.init(name: name, lifePoint: lifePoint, attack: attack)
+    }
+    convenience init(name: String) {
+        self.init(name: name, lifePoint: 75, attack: 20)
+    }
     
 }
 
 class Paladin: Character {
-    var lifePoint = 100
-    let attack = 6
-    let heal = 10
-    var typeClass = Class.paladin
+    
+    static let heal = 10
+    override init (name: String, lifePoint: Int, attack: Int) {
+        super.init(name: name, lifePoint: lifePoint, attack: attack)
+    }
+    convenience init(name: String) {
+        self.init(name: name, lifePoint: 100, attack: 7)
+    }
     
 }
-var arthas = Paladin(name: "Arthas")
-print(arthas.typeClass)
+
+var p1 = Player(playerName: "Yak")
+
+print("good")
+p1.heroes.append(Warrior(name: "Garrosh"))
+p1.heroes.append(Mage(name: "lol"))
+
+
+print(p1.heroes[0].attack)
+print(p1.heroes[1].attack)
+
+
+
+
+
